a11b7855d486c16da7834ea9553868aee80a468d
Yuki <khongbachofficial@gmail.com>
2026-05-28 19:10:29 +0700

    first commit

---
+++ src/main/java/com/mangakousei/mangakousei_backend/MangakouseiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mangakousei.mangakousei_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangakouseiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangakouseiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mangakousei/mangakousei_backend/MangakouseiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangakousei.mangakousei_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangakouseiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
