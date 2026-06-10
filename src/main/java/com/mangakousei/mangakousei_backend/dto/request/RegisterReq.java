0b1c558460ced6d1d81f9f462e1c7838b517e6df
d4rtwin <dacthinhhh@gmail.com>
2026-06-10 14:33:21 +0700

    Update RegisterReq.java

--- src/main/java/com/mangakousei/mangakousei_backend/dto/request/RegisterReq.java
+++ src/main/java/com/mangakousei/mangakousei_backend/dto/request/RegisterReq.java
@@ -2,7 +2,9 @@ package com.mangakousei.mangakousei_backend.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
+@Data
 public class RegisterReq {
     @NotBlank(message = "Full name cannot be blank")
     private String fullName;
@@ -13,28 +15,4 @@ public class RegisterReq {
 
     @NotBlank(message = "Password cannot be blank")
     private String password;
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
